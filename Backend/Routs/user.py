4fc0dbf36e0377637576a217c14682f39b9c6de7
Apishrana <apishrana682@gmail.com>
2026-07-23 16:59:42 +0530

    home page group menu

--- Backend/Routs/user.py
+++ Backend/Routs/user.py
@@ -31,7 +31,8 @@ async def groups_joined(currUser: User = Depends(getCurrentUser)):
         grp: GroupDB = await getGroup(i)
         u = []
         for i in grp.Users:
-            u.append(getUser(i).Name)
+            usr = getUser(i)
+            u.append(usr.Name)
         grp.Users = u
         returnData.append(grp)
     return {"Groups": returnData}
